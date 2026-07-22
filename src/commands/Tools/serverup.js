88e0293429eb4e2721701dbde67616d45172612c
peppa3109-byte <peppa3109@gmail.com>
2026-07-22 17:16:15 -0400

    Update serverup.js

--- src/commands/Tools/serverup.js
+++ src/commands/Tools/serverup.js
@@ -9,32 +9,24 @@ import { InteractionHelper } from '../../utils/interactionHelper.js';
 import { replyUserError, ErrorTypes } from '../../utils/errorHandler.js';
 import { logger } from '../../utils/logger.js';
 
-// Cambia esta URL por tu banner de "Server UP" (imgur, discord cdn, etc.)
-const DEFAULT_BANNER_URL = 'https://imgur.com/a/H3C8nNf';
-const DEFAULT_TITLE = 'ENVENENADO RP ESTA ABIERTO';
+const DEFAULT_BANNER_URL = 'https://i.imgur.com/0eOqo3B.png';
+const DEFAULT_TITLE = 'ENVENENADO ESTA ABIERTO';
 const DEFAULT_CONNECT = 'cfx.re/join/xlladb5';
-const DEFAULT_EXTRA = O ABRE <#1518421283946238103> si tienes problemas para ingresar
+const DEFAULT_EXTRA = 'O ABRE <#1518421283946238103> si tienes problemas para ingresar';
 
 export default {
   data: new SlashCommandBuilder()
     .setName('serverup')
-    .setDescription('Anuncia que el servidor está online')
+    .setDescription('Anuncia que el servidor esta online')
     .setDefaultMemberPermissions(PermissionFlagsBits.MentionEveryone)
     .addSubcommand((sub) =>
       sub
         .setName('enviar')
-        .setDescription('Envía el anuncio estándar de Server UP')
+        .setDescription('Envia el anuncio estandar de Server UP')
         .addChannelOption((opt) =>
           opt
             .setName('canal')
-            .setDescription('Canal donde se envía (por defecto este)')
-            .addChannelTypes(ChannelType.GuildText)
-            .setRequired(false),
-        )
-        .addChannelOption((opt) =>
-          opt
-            .setName('tickets')
-            .setDescription('Canal de tickets para mencionar')
+            .setDescription('Canal donde se envia (por defecto este)')
             .addChannelTypes(ChannelType.GuildText)
             .setRequired(false),
         ),
@@ -46,19 +38,19 @@ export default {
         .addStringOption((opt) =>
           opt
             .setName('titulo')
-            .setDescription('Título (ej: ENVENENADO IS UP)')
+            .setDescription('Titulo (ej: ENVENENADO IS UP)')
             .setRequired(true),
         )
         .addStringOption((opt) =>
           opt
             .setName('connect')
-            .setDescription('IP / código de conexión')
+            .setDescription('IP o codigo de conexion')
             .setRequired(true),
         )
         .addStringOption((opt) =>
           opt
             .setName('mensaje')
-            .setDescription('Texto extra (usa {tickets} para mencionar el canal de tickets)')
+            .setDescription('Texto extra del anuncio')
             .setRequired(false),
         )
         .addStringOption((opt) =>
@@ -70,14 +62,7 @@ export default {
         .addChannelOption((opt) =>
           opt
             .setName('canal')
-            .setDescription('Canal donde se envía')
-            .addChannelTypes(ChannelType.GuildText)
-            .setRequired(false),
-        )
-        .addChannelOption((opt) =>
-          opt
-            .setName('tickets')
-            .setDescription('Canal de tickets')
+            .setDescription('Canal donde se envia')
             .addChannelTypes(ChannelType.GuildText)
             .setRequired(false),
         )
@@ -106,52 +91,59 @@ export default {
       const sub = interaction.options.getSubcommand();
       const channel =
         interaction.options.getChannel('canal') || interaction.channel;
-      const ticketsChannel = interaction.options.getChannel('tickets');
 
       if (!channel?.isTextBased?.()) {
         return await replyUserError(interaction, {
           type: ErrorTypes.VALIDATION,
-          message: 'Elige un canal de texto válido.',
+          message: 'Elige un canal de texto valido.',
         });
       }
 
       const me = interaction.guild.members.me;
       const perms = channel.permissionsFor(me);
-      if (!perms?.has(['ViewChannel', 'SendMessages', 'EmbedLinks', 'MentionEveryone'])) {
+      if (
+        !perms?.has([
+          'ViewChannel',
+          'SendMessages',
+          'EmbedLinks',
+          'MentionEveryone',
+        ])
+      ) {
         return await replyUserError(interaction, {
           type: ErrorTypes.PERMISSION,
-          message: `No tengo permisos en ${channel} (Ver, Enviar, Embeds, Mencionar everyone).`,
+          message: `No tengo permisos en ${channel}.`,
         });
       }
 
-      let title, connect, extra, imageUrl, pingEveryone;
+      let title;
+      let connect;
+      let extra;
+      let imageUrl;
+      let pingEveryone;
 
-      if (sub === 'enviar') {
-        title = DEFAULT_TITLE;
-        connect = DEFAULT_CONNECT;
-        extra = DEFAULT_EXTRA;
-        imageUrl = DEFAULT_BANNER_URL;
-        pingEveryone = true;
-      } else {
-        title = interaction.options.getString('titulo');
-        connect = interaction.options.getString('connect');
-        extra =
-          interaction.options.getString('mensaje') ||
-          'O ABRE {tickets} si tienes problemas para ingresar';
-        imageUrl = interaction.options.getString('imagen') || DEFAULT_BANNER_URL;
-        pingEveryone = interaction.options.getBoolean('everyone') !== false;
-      }
-
-      const ticketsText = ticketsChannel ? `${ticketsChannel}` : '#Tickets';
-      const bodyExtra = extra.replace(/\{tickets\}/gi, ticketsText);
+if (sub === 'enviar') {
+  title = 'Envenenado RP';
+  connect = 'cfx.re/join/xlladb5';
+  extra = DEFAULT_EXTRA;
+  imageUrl = 'https://i.imgur.com/0eOqo3B.png';
+  pingEveryone = true;
+} else {
+  title = interaction.options.getString('titulo');
+  connect = interaction.options.getString('connect');
+  extra = interaction.options.getString('mensaje') || DEFAULT_EXTRA;
+  imageUrl = interaction.options.getString('imagen') || DEFAULT_BANNER_URL;
+  pingEveryone = interaction.options.getBoolean('everyone') !== false;
+}
 
       const description = [
-        `\`\`\`\n${connect}\n\`\`\``,
-        bodyExtra,
+        '```',
+        connect,
+        '```',
+        extra,
       ].join('\n');
 
       const embed = new EmbedBuilder()
-        .setColor(0x9B00FF)
+        .setColor(0x9b00ff)
         .setTitle(title)
         .setDescription(description)
         .setImage(imageUrl)
@@ -164,7 +156,7 @@ export default {
       });
 
       await InteractionHelper.safeEditReply(interaction, {
-        content: `✅ Server UP enviado en ${channel}`,
+        content: `Server UP enviado en ${channel}`,
       });
     } catch (error) {
       logger.error('serverup error:', error);
