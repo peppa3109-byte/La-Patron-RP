f9554161855bbe2e769a28a147e3bf48bea5cb33
peppa3109-byte <peppa3109@gmail.com>
2026-08-03 12:54:22 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,8 +23,8 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
-        state: "FiveM Community",     // this is what people actually see
+        name: "CasaB X", // required by Discord API, not shown in the client
+        state: "",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
